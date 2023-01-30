dd6b602f0ed3bee235fb13e8f2fdbda0eae0ac7d
alec247 <alec247@gmail.com>
2023-01-29 18:47:05 -0800

    submit solution for ex 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,21 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    //for loop for length of string, each time slice letter and push to empty array
+    //once loop completes, for loop and pop() each element back into string
+    //replaceAll() method to replace , with ''
+    let myArray = [];
+    let newString = '';
 
+    for (let i = 0; i < string.length; i++) {
+        myArray.push(string.substr(i, 1));
+    }
+
+    for (let i = 0; i < string.length; i++) {
+        newString += myArray.pop();
+    }
+
+    newString.replaceAll(/,/g, '');
+
+    return newString;
 };
 
 // Do not edit below this line
