c3f0cc5dc436a03366967df521967abaf0f7a8b6
alec247 <alec247@gmail.com>
2023-01-29 20:16:33 -0800

    submit solution for ex 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    // args can be accessed as any array (args[i])
+    for (let i = 0; i < args.length; i++) {
+        let index = array.indexOf(args[i]);
 
+        if (index > -1){
+            array.splice(index, 1);
+        }
+    }
+
+    return array;
 };
 
 // Do not edit below this line
