1081ff3f16461d38a0ff4af0aea2d32f50ebbcaf
alec247 <alec247@gmail.com>
2023-01-31 15:50:18 -0800

    submit solution for ex 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInput) {
+  // (tempInput − 32) × 5/9 = result
+  // must round to 1 decimal and work with negatives
+  let result = (tempInput - 32) * (5 / 9);
+
+  return Math.round(result*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInput) {
+  // (tempInput × 9/5 + 32) = result
+  let result = (tempInput * (9 / 5) + 32);
+
+  return Math.round(result*10)/10;
 };
 
 // Do not edit below this line
