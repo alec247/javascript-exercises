90fa23eb294636396e93c25f65908c1ed60ff2d6
alec247 <alec247@gmail.com>
2023-02-14 13:15:13 -0800

    Submit solution for ex 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str
+    .toLowerCase()
+    .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+    .replace(/\s/g, "");
+    let strSplit = str.split("");
+    let reverseArray = strSplit.reverse();
+    let reversedStr = reverseArray.join("");
 
+    if (reversedStr === str) {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
