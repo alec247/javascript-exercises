426309019d9b6a75ee331bbb2fd6e4a8598a6edf
alec247 <alec247@gmail.com>
2023-02-14 16:42:58 -0800

    Submit solution for ex 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    // negative input outputs "OOPS"
+    if(index <= 0) {
+        return "OOPS";
+    }
 
+    // convert string values to ints
+    if (typeof index === "string") {
+        index = Number(index);
+    }
+
+    // create initial array to push fib numbers to
+    let fibArray = [1, 1];
+
+    // run fib sequence for index iterations, push each number to fibArray
+    for (let i = 0; i < index; i++) {
+        if (fibArray[i]) continue;
+
+        let nextNum = fibArray[i - 1] + fibArray[i - 2];
+        fibArray.push(nextNum);
+    }
+
+    // returns correct value
+    return fibArray[index - 1];
 };
 
 // Do not edit below this line
