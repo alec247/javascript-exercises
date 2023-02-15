ba0f73f47cbbf792735b0451594753cf43e77a71
alec247 <alec247@gmail.com>
2023-02-14 17:01:44 -0800

    Submit solution for ex 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    people.sort((firstPerson, secondPerson) => {
+        if (!firstPerson.yearOfDeath) {
+            firstAge = new Date().getFullYear() - firstPerson.yearOfBirth;
+        } else {
+            firstAge = firstPerson.yearOfDeath - firstPerson.yearOfBirth;
+        }
 
+        if (!secondPerson.yearOfDeath) {
+            secondAge = new Date().getFullYear() - secondPerson.yearOfBirth;
+        } else {
+            secondAge = secondPerson.yearOfDeath - secondPerson.yearOfBirth;
+        }
+
+        return (firstAge > secondAge ? -1 : 1);
+    });
+
+    return people[0];
 };
 
 // Do not edit below this line
